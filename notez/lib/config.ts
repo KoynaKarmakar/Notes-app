9f2f8ecf2b72bf000bd3cef61f11bee9707d6ca5
KoynaKarmakar <koynakarmkar12@gmail.com>
2026-02-05 10:58:22 +0530

    make it work

--- notez/lib/config.ts
+++ notez/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE: string = 'http://127.0.0.1:5001' || ("https://notes-app-j3ds.onrender.com" as string);
+export const API_BASE: string = ("https://notes-app-j3ds.onrender.com" as string);
+//'http://127.0.0.1:5001'
